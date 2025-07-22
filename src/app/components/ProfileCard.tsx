cfc4d23ac7090c864ed59f20b352fe1709809175
Aziz Anwar <azisanw19@gmail.com>
2025-07-22 21:14:47 +0700

    fix: responseive mobile

--- src/app/components/ProfileCard.tsx
+++ src/app/components/ProfileCard.tsx
@@ -3,19 +3,22 @@ import Link from "next/link";
 
 export default function ProfileCard() {
   return (
-    <div className="flex gap-6 text-black">
-      <div className="flex-shrink-0">
+    <div className="flex flex-col sm:flex-row gap-6 text-black">
+      {/* Profile Image */}
+      <div className="flex-shrink-0 self-center sm:self-start">
         <Image
-          src="/profile.jpg" // Ganti dengan path gambar kamu
+          src="/profile.jpg"
           alt="Aziz Anwar"
           width={160}
           height={160}
           className="rounded-md"
         />
       </div>
-      <div className="ml-6 space-y-2">
-        <h1 className="text-3xl font-bold">AZIZ ANWAR</h1>
-        <p className="text-gray-500">
+
+      {/* Text Content */}
+      <div className="space-y-2">
+        <h1 className="text-3xl font-bold text-center sm:text-left">AZIZ ANWAR</h1>
+        <p className="text-gray-500 text-center sm:text-left">
           +6285701208132 | azisanw19@gmail.com |{' '}
           <Link href="https://linkedin.com/in/aziz-anwar/" target="_blank" className="underline">
             LinkedIn
@@ -31,7 +34,7 @@ export default function ProfileCard() {
           <br />
           Boyolali, Indonesia
         </p>
-        <p className="text-black text-sm">
+        <p className="text-black text-sm text-justify sm:text-left">
           Mobile Developer who has expertise in creating mobile applications using Kotlin with
           Declarative UI and Imperative UI. Apart from using the Kotlin programming language, I also
           have expertise in using the Dart language to create mobile applications. Understand
@@ -44,5 +47,6 @@ export default function ProfileCard() {
         </p>
       </div>
     </div>
+
   )
 }
